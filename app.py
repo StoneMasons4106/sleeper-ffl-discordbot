6e0a54c417c453f83620a26fc791c0b71ab4a60c
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-07-31 14:18:36 -0400

    Added on guild join message, requirements.txt.

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 import discord
 from discord.ext import commands
+import os
+from flask_pymongo import PyMongo
+import functions
+if os.path.exists("env.py"):
+    import env
+
+TOKEN = os.environ.get("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix='$')
+
+
+@bot.event
+async def on_guild_join(ctx):
+    await ctx.send('Happy to be here! Please run the add-league command to set your Sleeper Fantasy Football league!')
+
+
+bot.run(TOKEN)
