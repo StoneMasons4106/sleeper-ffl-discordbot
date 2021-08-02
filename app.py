133c7c2b502e909611389a8a2fe53deb1594597b
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-08-02 14:50:29 -0400

    Added trending players command using 10 as a limit over 24 hours.

--- app.py
+++ app.py
@@ -99,18 +99,18 @@ async def set_prefix(ctx, prefix: str):
 ## Set League ID in MongoDB
 
 @bot.command(name='add-league', help='Adds league associated to this guild ID.')
-async def add_league(ctx, league: str):
+async def add_league(ctx, league_id: str):
     if ctx.author.guild_permissions.administrator:
         existing_league = get_existing_league(ctx)
         if existing_league:
-            newvalue = {"$set": {"league": league}}
+            newvalue = {"$set": {"league": league_id}}
             MONGO.servers.update_one(existing_league, newvalue)
             embed = my_embed('Sleeper League Connection Status', 'Result of connection to Sleeper League request', discord.Colour.blue(), 'Connection Status', 'Successfully updated your Sleeper league to '+league+'!', False, ctx)
             await ctx.send(embed=embed)
         else:
             server_league_object = {
                 "server": str(ctx.message.guild.id),
-                "league": league
+                "league": league_id
             }
             MONGO.servers.insert_one(server_league_object)
             embed = my_embed('Sleeper League Connection Status', 'Result of connection to Sleeper League request', discord.Colour.blue(), 'Connection Status', 'Successfully updated your Sleeper league to '+league+'!', False, ctx)
@@ -154,6 +154,8 @@ async def my_league_members(ctx):
         await ctx.send(embed=embed)
 
 
+## Get League Standings Sorted by Most to Least Wins
+
 @bot.command(name='my-league-standings', help='Returns the current standings of my league with win loss record and points for.')
 async def my_league_standings(ctx):
     existing_league = get_existing_league(ctx)
@@ -173,6 +175,31 @@ async def my_league_standings(ctx):
         embed = my_embed('Sleeper League Standings', 'Display Current Standings of Sleeper League', discord.Colour.blue(), 'Standings', 'No league specified, run add-league command to complete setup.', False, ctx)
         await ctx.send(embed=embed)
 
+
+## Get Trending Players
+
+@bot.command(name='trending-players', help='Returns the current top 10 trending players over the last 24 hours based on add or drop rate.')
+async def trending_players(ctx, add_drop: str):
+    if add_drop == 'add' or add_drop == 'drop':
+        trending_players = sleeper_wrapper.Players().get_trending_players('nfl', add_drop, 24, 10)
+        trending_string = ''
+        count = 0
+        for player in trending_players:
+            count = count + 1
+            player_id = player["player_id"]
+            change = player["count"]
+            db_player = MONGO.players.find_one(
+                {"id": str(player_id)})
+            trending_string += str(count) + '.' + ' ' + db_player["name"] + ' ' + db_player["position"] + ' - ' + db_player["team"] + ' ' + str(change) + '\n'
+        if add_drop == 'add':
+            embed = my_embed('Trending Players', 'Display Current Trending Added Players', discord.Colour.blue(), 'Players', trending_string, False, ctx)
+            await ctx.send(embed=embed)
+        else:    
+            embed = my_embed('Trending Players', 'Display Current Trending Dropped Players', discord.Colour.blue(), 'Players', trending_string, False, ctx)
+            await ctx.send(embed=embed)
+    else:
+        await ctx.send('Invalid add_drop argument. Please use either add or drop to get trending players.')
+        
 
 # Bot Run
 
