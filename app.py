7392ff97fce9a16c7f53c7dadf789d8aa5ce2da5
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-08-11 16:30:09 -0400

    Fixed glitch with forecast and spaces in the argument.

--- app.py
+++ app.py
@@ -441,7 +441,7 @@ class Weather(commands.Cog, name='Weather'):
     ### Get Local Forecast
 
     @commands.command(name='forecast', help='Returns a 3 day forecast for any specific city specified.')
-    async def forecast(self, ctx, city: str):
+    async def forecast(self, ctx, *city: str):
         weather_api_key = os.environ.get("WEATHER_API_KEY")
         forecast = requests.get(
             'http://api.weatherapi.com/v1/forecast.json',
@@ -453,6 +453,15 @@ class Weather(commands.Cog, name='Weather'):
         )
         if forecast.status_code == 200:
             forecast_string=''
+            tuple_test = type(city) is tuple
+            if tuple_test:
+                city_string = ''
+                for word in city:
+                    print(word)
+                    city_string += f'{word} '
+                city = city_string
+            else:
+                pass
             for day in forecast.json()["forecast"]["forecastday"]:
                 forecast_string += f'{day["date"]}:\nHigh: {day["day"]["maxtemp_f"]} degrees F\nLow: {day["day"]["mintemp_f"]} degrees F\nWind: {day["day"]["maxwind_mph"]} mph\nPrecipitation Amount: {day["day"]["totalprecip_in"]} in.\nHumidity: {day["day"]["avghumidity"]}%\nChance of Rain: {day["day"]["daily_chance_of_rain"]}%\nChance of Snow: {day["day"]["daily_chance_of_snow"]}%\nGeneral Conditions: {day["day"]["condition"]["text"]}\n\n'
             embed = functions.my_embed('Weather Forecast', f'3 day forecast for {city}', discord.Colour.blue(), f'Forecast for {city}', forecast_string, False, ctx)
