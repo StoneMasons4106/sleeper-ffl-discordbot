f0744347e984fb8211e077d258aa14190c64710e
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-08-27 11:11:48 -0400

    Started some cleanup of app.py by moving the league cog. More to come.

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import functions
 import scheduled_jobs
 import requests
 import pendulum
+from sleeper_bot_commands import league
 from bs4 import BeautifulSoup as bs4
 if os.path.exists("env.py"):
     import env
@@ -264,150 +265,35 @@ class League(commands.Cog, name='League'):
 
     @commands.command(name='my-league')
     async def my_league_members(self, ctx):
-        existing_league = functions.get_existing_league(ctx)
-        if existing_league:
-            if "league" in existing_league:
-                league_id = existing_league["league"]
-                league = sleeper_wrapper.League(int(league_id)).get_league()
-                users_object = sleeper_wrapper.League(int(league_id)).get_users()
-                users = []
-                for user in users_object:
-                    users.append(user["display_name"])
-                embed = functions.my_embed('Sleeper League Info', 'Sleeper League General Information', discord.Colour.blue(), 'Name', f'[{league["name"]}](https://sleeper.app/leagues/{league_id})', False, ctx)
-                embed.add_field(name='Members', value=", ".join(users), inline=False)
-                embed.add_field(name='Quantity', value=len(users), inline=False)
-                embed.add_field(name='Trade Deadline', value=f"Week {league['settings']['trade_deadline']}", inline=False)
-                embed.add_field(name='Playoffs Start', value=f"Week {league['settings']['playoff_week_start']}", inline=False)
-                if "score_type" in existing_league:
-                    if existing_league["score_type"] == 'pts_ppr':
-                        embed.add_field(name='Scoring Type', value='PPR', inline=False)
-                    elif existing_league["score_type"] == 'pts_half_ppr':
-                        embed.add_field(name='Scoring Type', value='Half PPR', inline=False)
-                    elif existing_league["score_type"] == 'pts_std':
-                        embed.add_field(name='Scoring Type', value='Standard', inline=False)
-                else:
-                    pass
-                await ctx.send(embed=embed)
-            else:
-                embed = functions.my_embed('Sleeper League Info', 'Sleeper League Name and Member Info', discord.Colour.blue(), 'Members', 'No league specified, run add-league command to complete setup.', False, ctx)
-                await ctx.send(embed=embed)
-        else:
-            embed = functions.my_embed('Sleeper League Info', 'Sleeper League Name and Member Info', discord.Colour.blue(), 'Members', 'No league specified, run add-league command to complete setup.', False, ctx)
-            await ctx.send(embed=embed)
-
-
+        message = league.my_league_members(ctx)
+        await ctx.send(embed=message)
+    
     ### Get League Standings Sorted by Most to Least Wins
 
     @commands.command(name='my-league-standings')
     async def my_league_standings(self, ctx):
-        existing_league = functions.get_existing_league(ctx)
-        if existing_league:
-            if "league" in existing_league:
-                league_id = existing_league["league"]
-                users_object = sleeper_wrapper.League(int(league_id)).get_users()
-                rosters_object = sleeper_wrapper.League(int(league_id)).get_rosters()
-                filtered_roster_object = []
-                for roster in rosters_object:
-                    if roster["owner_id"] != None:
-                        filtered_roster_object.append(roster)
-                    else:
-                        pass
-                standings_object = sleeper_wrapper.League(int(league_id)).get_standings(filtered_roster_object, users_object)
-                standings_string = ''
-                count = 0
-                for i in standings_object:
-                    count = count + 1
-                    standings_string += f'{str(count)}. {i[0]} / Record: {i[1]}-{i[2]} / Points For: {i[3]}\n'
-                embed = functions.my_embed('Sleeper League Standings', 'Display Current Standings of Sleeper League', discord.Colour.blue(), 'Standings', standings_string, False, ctx)
-                await ctx.send(embed=embed)
-            else:
-                embed = functions.my_embed('Sleeper League Standings', 'Display Current Standings of Sleeper League', discord.Colour.blue(), 'Standings', 'No league specified, run add-league command to complete setup.', False, ctx)
-                await ctx.send(embed=embed)
-        else:
-            embed = functions.my_embed('Sleeper League Standings', 'Display Current Standings of Sleeper League', discord.Colour.blue(), 'Standings', 'No league specified, run add-league command to complete setup.', False, ctx)
-            await ctx.send(embed=embed)
+        message = league.my_league_standings(self, ctx)
+        await ctx.send(embed=message)
 
-    
     ### Get Current Week Matchups
 
     @commands.command(name='my-league-matchups')
     async def my_league_matchups(self, ctx, week: str):
-        if week.isnumeric():
-            if int(week) <= 18 and int(week) >= 1:
-                existing_league = functions.get_existing_league(ctx)
-                if existing_league:
-                    if "league" in existing_league:
-                        league_id = existing_league["league"]
-                        users = sleeper_wrapper.League(int(league_id)).get_users()
-                        rosters = sleeper_wrapper.League(int(league_id)).get_rosters()
-                        matchups = sleeper_wrapper.League(int(league_id)).get_matchups(int(week))
-                        if matchups:
-                            sorted_matchups = sorted(matchups, key=lambda i: i["matchup_id"])
-                            matchups_string = ''
-                            count = 0
-                            matchup_count = 1
-                            for matchup in sorted_matchups:
-                                count = count + 1
-                                roster = next((roster for roster in rosters if roster["roster_id"] == matchup["roster_id"]), None)
-                                user = next((user for user in users if user["user_id"] == roster["owner_id"]), None)
-                                if (count % 2) == 0:
-                                    matchup_count = matchup_count + 1
-                                    matchups_string += f'{user["display_name"]}\n'
-                                else:
-                                    matchups_string += f'{str(matchup_count)}. {user["display_name"]} vs. '
-                            embed = functions.my_embed('Current Week Matchups', f'Matchups for Week {week}', discord.Colour.blue(), 'Matchups', matchups_string, False, ctx)
-                            await ctx.send(embed=embed)
-                        else:
-                            await ctx.send('There are no matchups this week, try this command again during the season!')
-                    else:
-                        await ctx.send('Please run add-league command, no Sleeper League connected.')
-                else:
-                    await ctx.send('Please run add-league command, no Sleeper League connected.')
-            else:
-                await ctx.send('Invalid week number given. Choose a valid week between 1 and 18.')
-        else:
-            await ctx.send('Invalid week number given. Choose a valid week between 1 and 18.')
+        message = league.my_league_matchups(self, ctx, week)
+        if type(message) is str:
+           await ctx.send(message)
+        else: 
+            await ctx.send(embed=message)
 
-    
     ### Get Current Week Scoreboard
 
     @commands.command(name='my-league-scoreboard')
     async def my_league_scoreboard(self, ctx, week: str):
-        if week.isnumeric():
-            if int(week) <= 18 and int(week) >= 1:
-                existing_league = functions.get_existing_league(ctx)
-                if existing_league:
-                    if "league" in existing_league:
-                        league_id = existing_league["league"]
-                        users = sleeper_wrapper.League(int(league_id)).get_users()
-                        rosters = sleeper_wrapper.League(int(league_id)).get_rosters()
-                        matchups = sleeper_wrapper.League(int(league_id)).get_matchups(int(week))
-                        if matchups:
-                            sorted_matchups = sorted(matchups, key=lambda i: i["matchup_id"])
-                            scoreboard_string = ''
-                            count = 0
-                            matchup_count = 1
-                            for matchup in sorted_matchups:
-                                count = count + 1
-                                roster = next((roster for roster in rosters if roster["roster_id"] == matchup["roster_id"]), None)
-                                user = next((user for user in users if user["user_id"] == roster["owner_id"]), None)
-                                if (count % 2) == 0:
-                                    matchup_count = matchup_count + 1
-                                    scoreboard_string += f'{user["display_name"]} - {matchup["points"]}\n'
-                                else:
-                                    scoreboard_string += f'{str(matchup_count)}. {user["display_name"]} - {matchup["points"]} / '
-                            embed = functions.my_embed(f'Week {week} Scoreboard', f'Scoreboard for Week {str(week)}', discord.Colour.blue(), 'Scoreboard', scoreboard_string, False, ctx)
-                            await ctx.send(embed=embed)
-                        else:
-                            await ctx.send('There are no matchups this week, try this command again during the season!')
-                    else:
-                        await ctx.send('Please run add-league command, no Sleeper League connected.')
-                else:
-                    await ctx.send('Please run add-league command, no Sleeper League connected.')
-            else:
-                await ctx.send('Invalid week number given. Choose a valid week between 1 and 18.')
+        message = league.my_league_scoreboard(self, ctx, week)
+        if type(message) is str:
+            await ctx.send(message)
         else:
-            await ctx.send('Invalid week number given. Choose a valid week between 1 and 18.')
+            await ctx.send(embed=message)
 
 
 
