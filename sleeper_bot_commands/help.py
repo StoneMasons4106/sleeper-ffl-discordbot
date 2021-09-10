885948b5111b9b2cd88f5bddb870e457124a592f
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-09-10 10:59:32 -0400

    Moved important message to the top of help.

--- sleeper_bot_commands/help.py
+++ sleeper_bot_commands/help.py
@@ -18,7 +18,8 @@ def help(ctx):
     existing_prefix = MONGO.prefixes.find_one(
                 {"server": str(ctx.message.guild.id)})
     MONGO_CONN.close()
-    embed = functions.my_embed('Help', 'Use help <command> for detailed information.', discord.Colour.blue(), 'League', 'my-league, my-league-matchups, my-league-scoreboard, my-league-standings', False, ctx)
+    embed = functions.my_embed('Help', 'Use help <command> for detailed information.', discord.Colour.blue(), 'IMPORTANT:', 'Please make sure that the bot has access to application commands under the permissions found in the Sleeper-FFL role.', False, ctx)
+    embed.add_field(name='League', value='my-league, my-league-matchups, my-league-scoreboard, my-league-standings', inline=False)
     embed.add_field(name='Players', value='trending-players, roster, status, who-has', inline=False)
     embed.add_field(name='Weather', value='forecast, current-weather', inline=False)
     embed.add_field(name='Manage', value='kick, ban, unban', inline=False)
@@ -30,5 +31,4 @@ def help(ctx):
         embed.add_field(name='Prefix', value="$", inline=False)
     embed.add_field(name='Helpful Links', value="[Github](https://github.com/StoneMasons4106/sleeper-ffl-discordbot), [Top.gg](https://top.gg/bot/871087848311382086), [Patreon](https://www.patreon.com/stonemasons)", inline=False)
     embed.add_field(name='Interested in Becoming a Patron for Increased Functionality?', value='Click the link to Patreon in the Helpful Links section to get started.', inline=False)
-    embed.add_field(name='IMPORTANT:', value='Please make sure that the bot has access to application commands under the permissions found in the Sleeper-FFL role.', inline=False)
     return embed
