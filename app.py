a93e7285135d3bbbb743a7a3c3e84e2a8e05a4e5
StoneMasons4106 <jake.d.rubach@gmail.com>
2023-01-30 18:07:11 -0500

    Quick fix for find-user command erroring out if it can't find a matching discord user.

--- app.py
+++ app.py
@@ -303,12 +303,15 @@ async def find_user(ctx, *, member):
     message_author_id = str(ctx.author.id)
     if message_author_id == author_id:
         username, user_discriminator = member.split('#')
-        user_id = discord.utils.get(bot.get_all_members(), name=username, discriminator=user_discriminator).id
-        user = await bot.fetch_user(user_id)
-        shared_guilds = [guild for guild in bot.guilds if user in guild.members]
-        if shared_guilds:
-            await ctx.respond(f'{member} - {shared_guilds[0].id}', ephemeral=True)
-        else:
+        try:
+            user_id = discord.utils.get(bot.get_all_members(), name=username, discriminator=user_discriminator).id
+            user = await bot.fetch_user(user_id)
+            shared_guilds = [guild for guild in bot.guilds if user in guild.members]
+            if shared_guilds:
+                await ctx.respond(f'{member} - {shared_guilds[0].id}', ephemeral=True)
+            else:
+                await ctx.respond('No shared guilds found.', ephemeral=True)
+        except:
             await ctx.respond('No shared guilds found.', ephemeral=True)
     else:
         await ctx.respond('You do not have access to this command.', ephemeral=True)
