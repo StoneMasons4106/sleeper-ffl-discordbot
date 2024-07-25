884aed776ee26ae5ed19d7546fb1558f58b3b2f9
Jake Rubach <jake.d.rubach@gmail.com>
2024-07-24 23:42:05 -0400

    Fix for ngs and box-score commands for when no players are found.

--- sleeper_bot_commands/patron.py
+++ sleeper_bot_commands/patron.py
@@ -57,7 +57,7 @@ def ngs(ctx, bot, kind, player, year, week):
                 except:
                     player_filter = None
 
-                if player_filter.empty:
+                if player_filter is None:
                     embed = 'No stats were found with the given parameters.'
                 else:
                     if kind == 'passing':
@@ -111,7 +111,7 @@ def box_score(ctx, bot, kind, player, year, week):
                 except:
                     player_filter = None
 
-                if player_filter.empty:
+                if player_filter is None:
                     embed = 'No stats were found with the given parameters.'
                 else:
                     if kind == 'passing':
